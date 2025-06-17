02919bb9de24267a573abf09c7a755e423494617
Taylor <tm.96@live.com>
2025-06-17 10:55:35 -0400

    final commit

--- file_creator.py
+++ file_creator.py
@@ -1,4 +1,6 @@
 name = input("What's your name? \n")
+
 color = input("What's your favorite color? \n")
+
 with open('example.txt', 'w') as file:
-    file.write(f"{name} created this file")
+    file.write(f"{name} created this file, and their favorite color is {color}.")
